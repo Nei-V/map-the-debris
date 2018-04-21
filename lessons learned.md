c3a11fb9e48a92bd55a4b08f4610e3cc16786622
Ion Varsescu <19718355+Nei-V@users.noreply.github.com>
2018-04-21 18:58:29 +0300

    interactive form working

--- lessons learned.md
+++ lessons learned.md
@@ -1,2 +1,5 @@
 1. Forms
 Using forms with labels
+Adding new fields with javascript and templates
+Accessing the DOM (appendChild, createElement)
+name - naming convention for easy usage in backend
--- mapthedebris.js
+++ mapthedebris.js
@@ -2,6 +2,28 @@ function orbitalPeriod(arr) {
     var GM = 398600.4418;
     var earthRadius = 6367.4447;
     return arr;
-  }
-  
-  orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+}
+
+let i = 2;
+function addNewObject() {
+    let template = `
+      <h3>Enter name and altitude for object ${i}</h3>
+      <p>
+      <label>Object name</label>
+      <br>
+      <input name="flyingObject[${i}][object_name]">
+      </p>
+      <p>
+      <label>Objects's altitude</label>
+      <br>
+      <input name="flyingObject[${i}][object_altitude]>
+      </p>  
+      `;
+    let container = document.getElementById("objects-container");
+    let div = document.createElement("div");
+    div.innerHTML = template;
+    container.appendChild(div);
+    i++;
+};
+
+orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
